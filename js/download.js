93995863d238ec5c72038108914aa67fdc151986
Ovilia <zwl.sjtu@gmail.com>
2016-11-02 13:30:37 +0800

    map tool for echarts 3

--- js/download.js
+++ js/download.js
@@ -338,6 +338,45 @@ function downloadGeoJson(geojson) {
 
 }
 
+// 不压缩，下载地图文件
+function downloadGeoJs(geojson, mapName) {
+
+    var str = "(function (root, factory) {"
+        + "if (typeof define === 'function' && define.amd) {"
+        +     "define(['exports', 'echarts'], factory);"
+        + "} else if (typeof exports === 'object' "
+        + "&& typeof exports.nodeName !== 'string') {"
+        +     "factory(exports, require('echarts'));"
+        + "} else {"
+        +     "factory({}, root.echarts);"
+        + "}"
+        + "}(this, function (exports, echarts) {"
+        + "var log = function (msg) {"
+        +     "if (typeof console !== 'undefined') {"
+        +         "console && console.error && console.error(msg);"
+        +     "}"
+        + "};"
+        + "if (!echarts) {"
+        +     "log('ECharts is not Loaded');"
+        +     "return;"
+        + "}"
+        + "if (!echarts.registerMap) {"
+        +     "log('ECharts Map is not loaded');"
+        +     "return;"
+        + "}"
+        + "echarts.registerMap('" + mapName + "', "
+        + JSON.stringify(geojson) + ");}));";
+
+    var blob = new Blob([str], {
+        type: 'text/plain;charset=utf8'
+    });
+    var fileName = [];
+    fileName.push(fileStr);
+    fileName.push('js');
+    saveAs(blob, fileName.join('.'));
+
+}
+
 function compress(json) {
 
     json.UTF8Encoding = true;
